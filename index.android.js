81cb9ecc4151cfa574bc06b39a98286403f38534
leo <leo@altermyth.com>
2017-05-23 16:55:30 +0700

    init project

---
+++ index.android.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import App from './src/app';
+
+export default class HematQuApp extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+
+AppRegistry.registerComponent('HematQuApp', () => HematQuApp);
